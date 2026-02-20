290fa1969958f88f1651580af3a0dfe325a8a380
João Neto <joao.barrosn@upe.br>
2026-02-20 09:14:24 -0300

    chore: estrutura inicial

---
+++ src/test/java/com/ras/desafio_tarifa/DesafioTarifaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ras.desafio_tarifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTarifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
